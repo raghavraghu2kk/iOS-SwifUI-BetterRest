3bb0792573d62fa608830b7a332398414d787393
Raghavendra Mirajkar <raghavendramirajkar@RaghavendrasAir.lan>
2024-03-26 03:15:24 +0530

    Created the inital project files

---
+++ BetterRest-CoreML/BetterRest-CoreML/BetterRest_CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest_CoreMLApp.swift
+//  BetterRest-CoreML
+//
+//  Created by Raghavendra Mirajkar on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest_CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
